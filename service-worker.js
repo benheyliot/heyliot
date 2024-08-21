8032a0bff47dd7e5a74dcf36ed31490b671be6ec
benheyliot <benjamin@heyliot.com>
2024-08-21 16:29:35 +0200

    v3

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,39 @@
-const CACHE_NAME = 'photo-app-cache';
+const CACHE_NAME = 'offline-cache';
 const urlsToCache = [
-  '/heyliot/',  // Adjust if needed based on the actual path
-  '/heyliot/index.html',
-  '/heyliot/service-worker.js'
+  '/',
+  '/index.html',
+  // Ajoutez ici d'autres ressources (CSS, JS, images) que vous souhaitez mettre en cache
 ];
 
+// Le service worker s'installe.
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    .then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
+// Gestion des requêtes réseau.
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+    .then(response => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request).then(
+        newResponse => {
+          if (newResponse && newResponse.status === 200 && newResponse.type === 'basic') {
+            let responseClone = newResponse.clone();
+            caches.open(CACHE_NAME).then(cache => {
+              cache.put(event.request, responseClone);
+            });
+          }
+          return newResponse;
+        }
+      );
+    })
   );
 });
